48fc90c09f700e39ad7d9172431d5551b3890d9b
Lucas Jin <kblazer20@gmail.com>
2025-04-18 21:43:15 -0400

    Person joining/leaving tracking

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 A discord bot built to aid meetings. 
 
 ## Functionalities
-1. Discord voice call captions.
+1. Discord voice call captions. (shown in gui)
 2. Meeting topic summaries.
 3. Meeting activity summaries. (who joined, how long, what times)
--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,16 +1,46 @@
 // for when user joins or leaves vc
 
+const people = [];
+
 module.exports = {
     name: 'voiceStateUpdate',
     once: false,
     execute(client, oldState, newState) {
         if (oldState.channelId !== newState.channelId) {
-            if (newState.channel) {
-                const displayName = newState.member.nickname || newState.member.user.username;
+            if (newState.channel) { // person joins
+                const displayName = newState.member.user.username;
                 console.log(`${displayName} joined the voice channel ${newState.channel.name}`);
-            } else {
-                const displayName = oldState.member.nickname || oldState.member.user.username;
+
+                const person = people.find(person => person.name === displayName);
+                if (person) {
+                    person.joinTime.push(new Date());
+                } else {
+                    people.push({
+                        name: displayName,
+                        joinTime: [new Date()],
+                        leaveTime: [],
+                    });
+                }
+                console.log(people);
+            } else { // person leaves
+                const displayName = oldState.member.user.username;
                 console.log(`${displayName} left the voice channel ${oldState.channel.name}`);
+
+                const person = people.find(person => person.name === displayName);
+                if (person) {
+                    person.leaveTime.push(new Date());
+                } else {
+                    people.push({
+                        name: displayName,
+                        joinTime: [new Date()],
+                        leaveTime: [new Date()],
+                    });
+                }
+                console.log(people);
+                if (oldState.channel.members.size === 0) {
+                    console.log("Call ended");
+                    people.length = 0;
+                }
             }
         }
     }   
